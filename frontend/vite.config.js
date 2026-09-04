a175cefa578c16fa84fa4310f2f0092ce5e24894
prakharv19 <prakkharvarshney@gmail.com>
2026-09-04 21:04:46 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ["onefi-emi-frontend-mac9.onrender.com"],
+  },
 });
